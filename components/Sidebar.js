f6843419eed25e63de2a6b35fc4b6d2a25f50352
kevinokello <okellokevin77@gmail.com>
2022-10-14 15:26:12 +0300

    logo display on sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Sidebar() {
+  return (
+    <div>
+      {/* logo */}
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
+        <Image
+          width="50"
+          height="50"
+          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+        ></Image>
+      </div>
+      {/* menu */}
+
+      {/* button */}
+
+      {/* mini-profile */}
+    </div>
+  );
+}
